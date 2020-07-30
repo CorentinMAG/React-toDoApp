d95b9d3534223b35722b9cfaa3578692093ced0a
CorentinMAG <cor.mag91@gmail.com>
2020-07-30 11:13:05 +0200

    update

--- src/todoList.tsx
+++ src/todoList.tsx
@@ -87,7 +87,6 @@ export default class TodoList extends React.PureComponent<TodoListProps,TodoList
 
 	}
 	render () {
-		console.log('render')
 		let remainingCount = this.remainingCount
 		let completedCount = this.completedCount
 		let todosFiltered = this.state.todos.filter(FILTERS[this.state.filter])
